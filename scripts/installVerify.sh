69f77298ecd6f7bca2396dda3ad4980bbf022906
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-03-13 22:54:31 +0530

    Update installVerify.sh

--- scripts/installVerify.sh
+++ scripts/installVerify.sh
@@ -4,8 +4,7 @@ set -e
 varstr1=$(oc get pr -n mas-inst1-pipelines | awk -F' ' '{print $1}')
 varstr2=$(echo $varstr1 | cut -d '-' -f 3)
 
-# Function to track the status of each task run in the install pipeline and to exit in case of failure and to wait for 50 retries (with 180 seconds delay between each retry)
-  in case if the task is still running.
+# Function to track the status of each task run in the install pipeline and to exit in case of failure and to wait for 50 retries (with 180 seconds delay between each retry) in case if the task is still running.
 
 function verifyPipelineTaskStatus()
 {
@@ -32,21 +31,39 @@ for (( i=0; i<=50; i++ ));
 }
 
 verifyPipelineTaskStatus inst1-install-$varstr2-gencfg-workspace
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-pre-install-check
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-ibm-catalogs
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-cluster-monitoring
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-common-services
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-uds
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-cert-manager
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-mongodb
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-db2-manage
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-suite-dns
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-sls
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-suite-install
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-suite-config
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-suite-verify
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-suite-config-db2
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-suite-db2-setup-manage
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-app-install-manage
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-app-cfg-manage
+sleep 20
 verifyPipelineTaskStatus inst1-install-$varstr2-post-install-verify
