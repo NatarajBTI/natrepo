3a2b6910c27c5681e4ca977c55697aec0056c931
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-03-17 10:34:45 +0530

    Update installVerify.sh

--- scripts/installVerify.sh
+++ scripts/installVerify.sh
@@ -1,7 +1,7 @@
 set -e
 
 # Get the unique string in the pipeline name which got triggered for this install
-varstr1=$(oc get pr -n mas-inst3-pipelines | awk -F' ' '{print $1}')
+varstr1=$(oc get pr -n mas-inst4-pipelines | awk -F' ' '{print $1}')
 varstr2=$(echo $varstr1 | cut -d '-' -f 3)
 
 # Function to track the status of each task run in the install pipeline and to exit in case of failure and to wait for 50 retries (with 180 seconds delay between each retry) in case if the task is still running.
@@ -12,10 +12,10 @@ function verifyPipelineTaskStatus()
 
 for (( i=0; i<=50; i++ ));
 	do
-		varstr3=$(oc get taskrun $1 -n mas-inst3-pipelines | awk -F' ' '{print $3}')
+		varstr3=$(oc get taskrun $1 -n mas-inst4-pipelines | awk -F' ' '{print $3}')
         varstr3=$(echo $varstr3 | cut -d ' ' -f 1)
 
-        varstr4=$(oc get taskrun $1 -n mas-inst3-pipelines | awk -F' ' '{print $3}')
+        varstr4=$(oc get taskrun $1 -n mas-inst4-pipelines | awk -F' ' '{print $3}')
         varstr4=$(echo $varstr4 | cut -d ' ' -f 2)
 
         if [[ $varstr3 == "REASON" && $varstr4 == "Succeeded"  ]]; then
@@ -31,34 +31,34 @@ for (( i=0; i<=50; i++ ));
         done
 }
 
-verifyPipelineTaskStatus inst3-install-$varstr2-pre-install-check
+verifyPipelineTaskStatus inst4-install-$varstr2-pre-install-check
 sleep 20
-verifyPipelineTaskStatus inst3-install-$varstr2-gencfg-workspace
+verifyPipelineTaskStatus inst4-install-$varstr2-gencfg-workspace
 sleep 20
-verifyPipelineTaskStatus inst3-install-$varstr2-cluster-monitoring
+verifyPipelineTaskStatus inst4-install-$varstr2-cluster-monitoring
 sleep 20
-verifyPipelineTaskStatus inst3-install-$varstr2-ibm-catalogs
+verifyPipelineTaskStatus inst4-install-$varstr2-ibm-catalogs
 sleep 20
-verifyPipelineTaskStatus inst3-install-$varstr2-cluster-monitoring
+verifyPipelineTaskStatus inst4-install-$varstr2-cluster-monitoring
 sleep 20
-verifyPipelineTaskStatus inst3-install-$varstr2-common-services
+verifyPipelineTaskStatus inst4-install-$varstr2-common-services
 sleep 20
-verifyPipelineTaskStatus inst3-install-$varstr2-cert-manager
+verifyPipelineTaskStatus inst4-install-$varstr2-cert-manager
 sleep 20
-verifyPipelineTaskStatus inst3-install-$varstr2-uds
+verifyPipelineTaskStatus inst4-install-$varstr2-uds
 sleep 20
-verifyPipelineTaskStatus inst3-install-$varstr2-mongodb
+verifyPipelineTaskStatus inst4-install-$varstr2-mongodb
 sleep 20
-verifyPipelineTaskStatus inst3-install-$varstr2-db2-manage
+verifyPipelineTaskStatus inst4-install-$varstr2-db2-manage
 sleep 20
-verifyPipelineTaskStatus inst3-install-$varstr2-suite-dns
+verifyPipelineTaskStatus inst4-install-$varstr2-suite-dns
 sleep 20
-verifyPipelineTaskStatus inst3-install-$varstr2-sls
+verifyPipelineTaskStatus inst4-install-$varstr2-sls
 sleep 20
-verifyPipelineTaskStatus inst3-install-$varstr2-suite-install
+verifyPipelineTaskStatus inst4-install-$varstr2-suite-install
 sleep 20
-verifyPipelineTaskStatus inst3-install-$varstr2-suite-config
+verifyPipelineTaskStatus inst4-install-$varstr2-suite-config
 sleep 20
-verifyPipelineTaskStatus inst3-install-$varstr2-suite-verify
+verifyPipelineTaskStatus inst4-install-$varstr2-suite-verify
 sleep 20
-verifyPipelineTaskStatus inst3-install-$varstr2-post-install-verify
+verifyPipelineTaskStatus inst4-install-$varstr2-post-install-verify
