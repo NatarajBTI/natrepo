6fc532eb00afddb9b1c9a6b57b327f90dd629cce
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-03-19 09:00:03 +0530

    Update installVerify.sh

--- scripts/installVerify.sh
+++ scripts/installVerify.sh
@@ -2,24 +2,24 @@ set -e
 
 # Function to track the status of pipeline and to exit in case of failure and to wait for 50 retries (with 180 seconds delay between each retry) in case if the pipeline is still running.
 
-#sleep 300
+sleep 100
 
 function verifyPipelineStatusManage()
 {
 
 for (( i=0; i<=50; i++ ));
         do
-                varstr3=$(oc get pr -n mas-inst4-pipelines | awk -F' ' '{print $3}')
+                varstr3=$(oc get pr -n mas-${var2}-pipelines | awk -F' ' '{print $3}')
         varstr3=$(echo $varstr3 | cut -d ' ' -f 1)
 
-        varstr4=$(oc get pr -n mas-inst4-pipelines | awk -F' ' '{print $3}')
+        varstr4=$(oc get pr -n mas-${var2}-pipelines | awk -F' ' '{print $3}')
         varstr4=$(echo $varstr4 | cut -d ' ' -f 2)
 
         if [[ $varstr3 == "REASON" && $varstr4 == "Succeeded"  ]]; then
                 echo "Pipeline run is successful"
                 break
         elif [[ $varstr3 == "REASON" && $varstr4 == "Running"  ]]; then
-                echo "Task run is still running"
+                echo "Pipeline run is still running"
                 sleep 180
         else
                 echo "Pipeline run failed"
@@ -33,17 +33,17 @@ function verifyPipelineStatusCore()
 
 for (( i=0; i<=20; i++ ));
         do
-                varstr3=$(oc get pr -n mas-inst4-pipelines | awk -F' ' '{print $3}')
+                varstr3=$(oc get pr -n mas-${var2}-pipelines | awk -F' ' '{print $3}')
         varstr3=$(echo $varstr3 | cut -d ' ' -f 1)
 
-        varstr4=$(oc get pr -n mas-inst4-pipelines | awk -F' ' '{print $3}')
+        varstr4=$(oc get pr -n mas-${var2}-pipelines | awk -F' ' '{print $3}')
         varstr4=$(echo $varstr4 | cut -d ' ' -f 2)
 
         if [[ $varstr3 == "REASON" && $varstr4 == "Succeeded"  ]]; then
                 echo "Pipeline run is successful"
                 break
         elif [[ $varstr3 == "REASON" && $varstr4 == "Running"  ]]; then
-                echo "Task run is still running"
+                echo "Pipeline run is still running"
                 sleep 180
         else
                 echo "Pipeline run failed"
@@ -51,9 +51,11 @@ for (( i=0; i<=20; i++ ));
         fi
         done
 }
-
 var1=$1
-echo $1
+var2=$2
+echo "Deployment flavour is:" $1
+echo "Instance Id is:" $2
+
 if [[ $var1=="core" ]]; then
 verifyPipelineStatusCore
 elif [[ $var1=="manage" ]]; then
