a49adcafe5fe9611b43b8a595cb056d542a9b84b
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-03-19 09:12:40 +0530

    Update getAdminURL.sh

--- scripts/getAdminURL.sh
+++ scripts/getAdminURL.sh
@@ -1,3 +1,27 @@
-varstr=$(oc get route -n mas-inst4-core --no-headers | grep 'admin.inst4' | awk '{print $2}')
+set -e
+
+function getAdminURLCore()
+{
+varstr=$(oc get route -n mas-${var2}-core --no-headers | grep 'admin.${var2}' | awk '{print $2}')
+varstr="https://"$varstr""
+echo -n '{"admin_url":"'"${varstr}"'"}'
+}
+
+function getAdminURLManage()
+{
+varstr=$(oc get route -n mas-${var2}-manage --no-headers | grep 'wrkid2-all.manage' | awk '{print $2}')
 varstr="https://"$varstr"/maximo"
 echo -n '{"admin_url":"'"${varstr}"'"}'
+}
+
+var1=$1
+var2=$2
+echo "Deployment flavour is:" $1
+echo "Instance Id is:" $2
+
+if [[ $var1=="core" ]]; then
+getAdminURLCore
+else [[ $var1=="manage" ]]; then
+getAdminURLManage
+fi
+
