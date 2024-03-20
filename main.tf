eb6c6c809023031953262bc4d74df6af9d13193a
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-03-20 20:59:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ provisioner "local-exec" {
 
 data "external" "maximo_admin_url" {
 
-  program    = ["/bin/bash", "${path.module}/scripts/getAdminURL.sh ${var.deployment_flavour} ${var.mas_instance_id} ${var.mas_workspace_id}"]
+  program    = ["/bin/bash", "-c", "${path.module}/scripts/getAdminURL.sh ${var.deployment_flavour} ${var.mas_instance_id} ${var.mas_workspace_id}"]
   query = {
     KUBECONFIG   = data.ibm_container_cluster_config.cluster_config.config_file_path
   }
