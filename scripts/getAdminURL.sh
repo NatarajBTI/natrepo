69279161d3cfff25ddcb626c04b50477661fffd2
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-03-25 14:37:26 +0530

    Update getAdminURL.sh

--- scripts/getAdminURL.sh
+++ scripts/getAdminURL.sh
@@ -4,14 +4,16 @@ function getAdminURLCore()
 {
 varstr=$(oc get route -n mas-${var2}-core --no-headers | grep admin.${var2} | awk '{print $2}')
 varstr="https://"$varstr""
-echo -n '{"admin_url":"'"${varstr}"'"}'
+echo -n '{"admin_url":"'"${varstr}"'"}' > url.txt
+chmod +x url.txt
 }
 
 function getAdminURLManage()
 {
 varstr=$(oc get route -n mas-${var2}-manage --no-headers | grep ${var3}-all.manage | awk '{print $2}')
 varstr="https://"$varstr"/maximo"
-echo -n '{"admin_url":"'"${varstr}"'"}'
+echo -n '{"admin_url":"'"${varstr}"'"}' > url.txt
+chmod +x url.txt
 }
 
 var1=$1
