aeb95fccbd31570c44e269653fc9aebfa2b17016
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-03-25 14:43:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -98,6 +98,18 @@ provisioner "local-exec" {
   depends_on = [time_sleep.wait_300_seconds]
 }
 
+resource "null_resource" "admin_url" {
+
+provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = "${path.module}/scripts/getAdminURL.sh ${var.deployment_flavour} ${var.mas_instance_id} ${var.mas_workspace_id}"
+	environment = {
+      KUBECONFIG = data.ibm_container_cluster_config.cluster_config.config_file_path
+    }
+  }
+  depends_on = [null_resource.install_verify]
+}
+
 data "external" "get_pipeline_result" {
 
   program    = ["/bin/bash", "-c", "${path.module}/scripts/getResult.sh"]
@@ -107,11 +119,11 @@ data "external" "get_pipeline_result" {
 depends_on = [null_resource.install_verify]
 }
 
-data "external" "maximo_admin_url" {
+data "external" "get_admin_url" {
 
-  program    = ["/bin/bash", "-c", "${path.module}/scripts/getAdminURL.sh ${var.deployment_flavour} ${var.mas_instance_id} ${var.mas_workspace_id}"]
+  program    = ["/bin/bash", "-c", "${path.module}/scripts/getAdminURL.sh"]
   query = {
     KUBECONFIG   = data.ibm_container_cluster_config.cluster_config.config_file_path
   }
-  depends_on = [null_resource.install_verify]
+depends_on = [null_resource.admin_url]
 }
