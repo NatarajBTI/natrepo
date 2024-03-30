535dbeab47cc5599b9dbf87c5f96fdf199b2cacf
NATARAJSHIVASHANKARA <NATARAJSHIVASHANKARA@LAPTOP-69F31GB6>
2024-03-30 20:56:43 +0530

    changes..

--- scripts/getAdminURL.sh
+++ scripts/getAdminURL.sh
@@ -4,7 +4,10 @@ function getAdminURLCore()
 {
 varstr=$(oc get route -n mas-${var2}-core --no-headers | grep admin.${var2} | awk '{print $2}')
 varstr="https://"$varstr""
-echo -n '{"admin_url":"'"${varstr}"'"}' > url.txt
+jq --arg key0   'admin_url' \
+   --arg value0 $varstr \
+   '. | .[$key0]=$value0' \
+  <<<'{}' > url.txt
 chmod +x url.txt
 }
 
