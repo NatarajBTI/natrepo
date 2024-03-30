cb7bc5bb0cf8b136fb62d80a7729b848a0f8b3d9
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-03-30 18:04:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,27 +8,9 @@ variable "ibmcloud_api_key" {
   sensitive   = true
 }
 
-variable "region" {
-  description = "Cluster region"
-  type        = string
-  nullable    = false
-}
-
 variable "mas_instance_id" {
   type        = string
   description = "Enter the MAS instance Id"
-  nullable    = false
-}
-
-variable "mas_workspace_id" {
-  type        = string
-  description = "Enter the workspace Id"
-  nullable    = false
-}
-
-variable "mas_workspace_name" {
-  type        = string
-  description = "Enter the workspace name"
   nullable    = false
 }
 
@@ -38,55 +20,11 @@ variable "deployment_flavour" {
   nullable    = false
 }
 
-variable "sls_license_id" {
-  type        = string
-  description = "Enter the SLS license ID"
-  sensitive   = true
-  nullable    = false
-}
-
-variable "uds_contact_email" {
-  type        = string
-  description = "Enter the email ID for UDS"
-  nullable    = false
-}
-
-variable "uds_contact_firstname" {
-  type        = string
-  description = "Enter your first name to be used in UDS"
-  nullable    = false
-}
-
-variable "uds_contact_lastname" {
-  type        = string
-  description = "Enter your last name to be used in UDS"
-  nullable    = false
-}
-
-variable "mas_entitlement_key" {
-  description = "Entitlement key to access MAS Image registry"
-  type        = string
-  sensitive   = true
-}
-
-variable "mas_license" {
-  description = "MAS License file content"
-  type        = string
-  sensitive   = true
-}
-
 variable "cluster_id" {
   type        = string
   description = "Id of the target IBM Cloud OpenShift Cluster"
   nullable    = false
 }
-
-# variable "resource_group" {
-#   type        = string
-#   description = "Resource group to provision the cluster in"
-#   default     = null
-# }
-
 
 variable "cluster_config_endpoint_type" {
   description = "Specify which type of endpoint to use for for cluster config access: 'default', 'private', 'vpe', 'link'. 'default' value will use the default endpoint of the cluster."
