e9058cdc697fe06c0dacabb7ae97e4ce12cf25f1
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-03-31 12:15:24 +0530

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -4,6 +4,7 @@ import os
 def getAdminURLCore(dep_flavor, instid, wrksid):
     varstr = os.popen('oc get route -n mas-' + instid + '-core --no-headers | grep admin.' + instid + ' | awk \'{print $2}\'').read().strip()
     url = "https://" + varstr
-    print(json.dumps({ 'admin_url': url }, ensure_ascii=False))
-
-getAdminURLCore(dep_flavor = "core", instid = "natinst2", wrksid = "wrkid2")
+    json_object = { 'admin_url': url }
+    print(json_object)
+   
+getAdminURLCore(dep_flavor="core", instid="natinst2", wrksid="wrkid2")
