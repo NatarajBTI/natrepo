55e8c6183b48703d83c1cd9cd8067e46bfceb9c5
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-04-02 06:24:51 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@
 
 output "maximo_admin_url" {
   description = "Admin URL of MAS/Manage application is"
-  value       = data.external.get_admin_url.result.admin_url
+  value       = data.external.get_admin_url.result
 }
 
 output "pipeline_execution_status" {
