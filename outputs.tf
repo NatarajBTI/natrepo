534894f06e0fefd06b13052014cc33f3147aa184
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-04-06 15:52:20 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -4,5 +4,5 @@
 
 output "maximo_admin_url" {
   description = "Admin URL of Manage application is"
-  value       = jsonencode(data.external.maximo_admin_url.result.admin_url)
+  value       = data.external.maximo_admin_url.result.admin_url
 }
