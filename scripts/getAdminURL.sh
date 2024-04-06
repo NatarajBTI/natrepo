cee58aaa1d165a01c1df9fcb19f38b47f56fb7a4
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-04-06 19:17:50 +0530

    Update getAdminURL.sh

--- scripts/getAdminURL.sh
+++ scripts/getAdminURL.sh
@@ -4,7 +4,7 @@ function getAdminURLCore()
 {
 varstr=$(oc get route -n mas-${var2}-core --no-headers | grep admin.${var2} | awk '{print $2}')
 #varstr="https://"$varstr""
-echo -n '{"admin_url"="'"${varstr}"'"}'
+echo -n '"admin_url"="'"${varstr}"'"'
 }
 
 function getAdminURLManage()
