3c0317f77a6ff7ee804a786322a5a1407f3e13ff
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-04-06 17:52:51 +0530

    Create testscript.sh

---
+++ scripts/testscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Sample JSON-encoded map
+json_string='{"admin_url": "value1"}'
+
+# Parse JSON string and iterate over key-value pairs
+echo "$json_string" | jq -r 'to_entries[] | "\(.key) : \(.value)"'
