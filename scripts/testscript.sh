745156ec371eb882e27c328e890f0010e4a0ad29
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-04-06 18:28:10 +0530

    Update testscript.sh

--- scripts/testscript.sh
+++ scripts/testscript.sh
@@ -5,4 +5,4 @@ json_string='{"admin_url": "value1"}'
 
 # Parse JSON string and iterate over key-value pairs
 #echo "$json_string" | jq -r 'to_entries[] | "\(.key) : \(.value)"'
-echo -n '{"admin_url":"testurl"}'
+echo -n '{"admin_url":"https://admin.natinst3.masdaub05-workload-cluste-6f1620198115433da1cac8216c06779b-0000.eu-de.containers.appdomain.cloud"}'
