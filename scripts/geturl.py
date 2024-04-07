d20d33dc4574666c8f00c14598c8b67ea9aab7c7
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-04-08 04:15:01 +0530

    Update geturl.py

--- scripts/geturl.py
+++ scripts/geturl.py
@@ -1,12 +1,17 @@
 import json
+import os
 
-# Create a dictionary with string keys and string values
-data = {
-    "admin_url": "value1"
-}
+def getAdminURLCore(dep_flavor, instid, wrksid):
+    varstr = os.popen('oc get route -n mas-' + instid + '-core --no-headers | grep admin.' + instid + ' | awk \'{print $2}\'').read().strip()
+    varstr = "c104-e.br-sao.containers.cloud.ibm.com:31295"
+    url = "https://" + varstr
+    data = {
+    "admin_url": url
+    }
+    # Encode the dictionary into JSON format
+    json_output = json.dumps(data)
 
-# Encode the dictionary into JSON format
-json_output = json.dumps(data)
+    # Print the JSON output
+    print(json_output)
 
-# Print the JSON output
-print(json_output)
+getAdminURLCore(dep_flavor="core", instid="natinst1", wrksid="wrkid1")
