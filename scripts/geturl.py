81516037df409e7c221b797fa1da3ab818dd135a
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-04-08 08:43:36 +0530

    Update geturl.py

--- scripts/geturl.py
+++ scripts/geturl.py
@@ -5,8 +5,6 @@ import os
 def getAdminURLCore(dep_flavor, instid, wrksid):
     varstr = os.popen('oc get route -n mas-' + instid + '-core --no-headers | grep admin.' + instid + ' | awk \'{print $2}\'').read().strip()    
     
-    #if varstr:  
-        #url = "https://" + varstr
         data = {
             "admin_url": varstr
         }
