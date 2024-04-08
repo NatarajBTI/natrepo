412728a665d7036694531cea1b29f1018d04a098
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-04-08 08:37:53 +0530

    Update geturl.py

--- scripts/geturl.py
+++ scripts/geturl.py
@@ -1,13 +1,14 @@
+
 import json
 import os
 
 def getAdminURLCore(dep_flavor, instid, wrksid):
     varstr = os.popen('oc get route -n mas-' + instid + '-core --no-headers | grep admin.' + instid + ' | awk \'{print $2}\'').read().strip()    
     
-    if varstr:  # Check if varstr is not empty
-        url = "https://" + varstr
+    #if varstr:  
+        #url = "https://" + varstr
         data = {
-            "admin_url": url
+            "admin_url": varstr
         }
         # Encode the dictionary into JSON format
         json_output = json.dumps(data)
