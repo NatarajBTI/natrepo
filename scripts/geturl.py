2e92e8f9e0554eea190da5aed06ba127627b61d3
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-04-08 08:53:18 +0530

    Update geturl.py

--- scripts/geturl.py
+++ scripts/geturl.py
@@ -5,13 +5,13 @@ import os
 def getAdminURLCore(dep_flavor, instid, wrksid):
     varstr = os.popen('oc get route -n mas-' + instid + '-core --no-headers | grep admin.' + instid + ' | awk \'{print $2}\'').read().strip()    
     
-        data = {
-            "admin_url": varstr
+    data = {
+        "admin_url": varstr
         }
-        # Encode the dictionary into JSON format
-        json_output = json.dumps(data)
+    # Encode the dictionary into JSON format
+    json_output = json.dumps(data)
 
-        # Print the JSON output
-        print(json_output)
+    # Print the JSON output
+    print(json_output)
 
 getAdminURLCore(dep_flavor="core", instid="natinst1", wrksid="wrkid1")
