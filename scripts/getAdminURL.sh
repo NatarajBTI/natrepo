c492b02cdc5f372e8e9aef0768d06f255dff3c32
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-04-09 22:03:34 +0530

    Update getAdminURL.sh

--- scripts/getAdminURL.sh
+++ scripts/getAdminURL.sh
@@ -1,26 +1,83 @@
-set -e
+import json
+import subprocess
+import sys
+import time
 
-function getAdminURLCore()
-{
-varstr=$(oc get route -n mas-${var2}-core --no-headers | grep admin.${var2} | awk '{print $2}')
-varstr="https://"$varstr""
-echo -n '{"admin_url":"'"${varstr}"'"}'
-}
+def verifyPipelineStatus(kube_config, instid, capability):
+    RETRY_COUNT = 0
+    TIME_TO_WAIT = 0
+    if capability == "core":
+        RETRY_COUNT = 30
+        TIME_TO_WAIT = 180
+    elif capability == "manage":
+        RETRY_COUNT = 60
+        TIME_TO_WAIT = 300
+    
+    
+      
+    try:
+        pipline_status= "Failed Retrying, Pipeline still in Running state "
+        for interval_count in range(RETRY_COUNT):
+            process = subprocess.Popen(['oc', 'get', 'pr',
+                                        '-n', f'mas-{instid}-pipelines',
+                                        '-o', 'json',
+                                        '--kubeconfig', kube_config],
+                                    stdout=subprocess.PIPE, universal_newlines=True)
 
-function getAdminURLManage()
-{
-varstr=$(oc get route -n mas-${var2}-manage --no-headers | grep ${var3}-all.manage | awk '{print $2}')
-varstr="https://"$varstr"/maximo"
-echo -n '{"admin_url":"'"${varstr}"'"}'
-}
+            output, _ = process.communicate()
 
-var1=$1
-var2=$2
-var3=$3
+            if process.returncode != 0:
+                print("Error: Failed to execute 'oc get route' command")
+                sys.exit(1)
 
-if [[ $var1 == "core" ]]; then
-getAdminURLCore
-elif [[ $var1 == "manage" ]]; then
-getAdminURLManage
-fi
+            data = json.loads(output)
+            pipeline_runs = data.get('items', [])
+            
+            pipeline_run = None
+            
+            if len(pipeline_runs) > 0:
+                pipeline_run = pipeline_runs[0]
+            
+            if pipeline_run:
 
+                pipeline_status_reason = pipeline_run.get('status')[0].get('reason')
+                
+                if pipeline_status_reason == "Completed":
+                    pipline_status = "Successful"
+                    break
+                    
+                elif pipeline_status_reason == "Running":
+					print("Pipeline is still running")
+                    time.sleep(TIME_TO_WAIT)
+                    pass
+                elif pipeline_status_reason == "Failed":
+                    pipline_status = pipeline_run.get('status')[0].get('message')
+                    break
+        result = {
+            "PipelineRunStatus":pipline_status
+            }
+        json_output = json.dumps(result)
+        print(json_output)
+
+    except json.JSONDecodeError as e:
+        print(f"Error: Failed to parse JSON: {e}")
+        sys.exit(3)
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error: Command '{e.cmd}' returned non-zero exit status {e.returncode}")
+        sys.exit(4)
+
+    except OSError as e:
+        print(f"Error: Failed to execute command: {e}")
+        sys.exit(5)
+
+
+if __name__ == "__main__":
+    # get KUBECONFIG containing path from json passed to the command as an argument
+    # Read the JSON input from stdin
+    input_json = json.loads(sys.stdin.read())
+
+    # get the KUBECONFIG path from the json
+    kubeconfig = input_json['KUBECONFIG']
+    
+    verifyPipelineStatus(kube_config=kubeconfig, instid="natinst1",capability="core")
