c5dac634d35a099022a429bd65aef0e8c708284f
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-04-20 10:20:47 +0530

    Update pipelineVerify.sh

--- scripts/pipelineVerify.sh
+++ scripts/pipelineVerify.sh
@@ -1,16 +1,14 @@
 var1=$1
- do
         varstr3=$(oc get pr -n mas-${var1}-pipelines | awk -F' ' '{print $3}')
         varstr3=$(echo $varstr3 | cut -d ' ' -f 1)
 
         varstr4=$(oc get pr -n mas-${var1}-pipelines | awk -F' ' '{print $3}')
         varstr4=$(echo $varstr4 | cut -d ' ' -f 2)
 
-        if [[ $varstr3 == "REASON" && $varstr4 == "Completed"  ]]; then                
+        if [[ $varstr3 == "REASON" && $varstr4 == "Completed"  ]]; then
                 break
         elif [[ $varstr3 == "REASON" && $varstr4 == "Running"  ]]; then
                 exit
         elif [[ $varstr3 == "REASON" && $varstr4 == "Failed"  ]]; then
-        	      exit
+                      exit
         fi
-        done
