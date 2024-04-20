b6e29a56a0500e95985ff5ae24b060d6f867eb94
Nataraj <57617912+NatarajBTI@users.noreply.github.com>
2024-04-20 07:42:08 +0530

    Update getAdminURL.py

--- scripts/getAdminURL.py
+++ scripts/getAdminURL.py
@@ -5,105 +5,112 @@ import sys
 
 def getAdminURLCore(kube_config, instid):
     try:
-        result = {"admin_url": ""}        
-        process = subprocess.Popen(
-            [
-                "oc",
-                "get",
-                "route",
-                "-n",
-                f"mas-{instid}-core",
-                "-o",
-                "json",
-                "--kubeconfig",
-                kube_config,
-            ],
-            stdout=subprocess.PIPE,
-            universal_newlines=True,
-        )
+        process = subprocess.Popen(['oc', 'get', 'route',
+                                    '-n', f'mas-{instid}-core',
+                                    '-o', 'json',
+                                    '--kubeconfig', kube_config],
+                                   stdout=subprocess.PIPE, universal_newlines=True)
 
         output, _ = process.communicate()
 
         if process.returncode != 0:
-            varstr = ""
-            result["admin_url"] = varstr
-            json_output = json.dumps(result)
-            return json_output
+            print(json.dumps({
+                "error":f"Failed to execute 'oc get route' command"
+            }))
+            return
 
         data = json.loads(output)
-        routes = data.get("items", [])
-
-        for route in routes:
-            if f"admin.{instid}" in route["spec"]["host"]:
-                varstr = route["spec"]["host"]
-                break
-        if varstr != "":
-            result["admin_url"] = f"https://{varstr}"
-        else:
-            varstr = ""
-            result["admin_url"] = varstr
-
-        json_output = json.dumps(result)
-        print(json_output)
-
-    except Exception as e:
-        print(e)
+        routes = data.get('items', [])
         varstr = ""
-        result["admin_url"] = varstr
+        for route in routes:
+            if f'admin.{instid}' in route['spec']['host']:
+                varstr = route['spec']['host']
+                break
+        else:
+            print(json.dumps({
+                "error":f"Error: No route found for 'admin.{instid}'"
+            }))
+            return
+
+        result = {
+            "admin_url": f"https://{varstr}"
+        }
         json_output = json.dumps(result)
         print(json_output)
 
+    except json.JSONDecodeError as e:
+        error = {"error":f"Error: Failed to parse JSON: {e}"}
+        json_error = json.dumps(error)
+        print(json_error)
 
-def getAdminURLManage(kube_config, instid, workspaceId):
+    except subprocess.CalledProcessError as e:
+        error = {
+            "error": f"Error: Command '{e.cmd}' returned non-zero exit status {e.returncode}"
+        }
+        json_error = json.dumps(error)
+        print(json_error)
+
+    except OSError as e:
+        error = {
+            "error" : f"Error: Failed to execute command: {e}"
+        }
+        json_error = json.dumps(error)
+        print(json_error)
+
+def getAdminURLManage(kube_config, instid,workspaceId):
     try:
-        result = {"admin_url": ""}        
-        process = subprocess.Popen(
-            [
-                "oc",
-                "get",
-                "route",
-                "-n",
-                f"mas-{instid}-manage",
-                "-o",
-                "json",
-                "--kubeconfig",
-                kube_config,
-            ],
-            stdout=subprocess.PIPE,
-            universal_newlines=True,
-        )
+        process = subprocess.Popen(['oc', 'get', 'route',
+                                    '-n', f'mas-{instid}-manage',
+                                    '-o', 'json',
+                                    '--kubeconfig', kube_config],
+                                   stdout=subprocess.PIPE, universal_newlines=True)
 
         output, _ = process.communicate()
 
         if process.returncode != 0:
-            varstr = ""
-            result["admin_url"] = varstr
-            json_output = json.dumps(result)
-            return json_output
+            print(json.dumps({
+                "error":"Failed to execute 'oc get route' command"
+            }))
+            return
 
         data = json.loads(output)
-        routes = data.get("items", [])
-
-        for route in routes:
-            if f"{workspaceId}-all.manage.{instid}" in route["spec"]["host"]:
-                varstr = route["spec"]["host"]
-                break
-
-        if varstr != "":
-            result["admin_url"] = f"https://{varstr}/maximo"
-        else:
-            varstr = ""
-            result["admin_url"] = varstr
-
-        json_output = json.dumps(result)
-        print(json_output)
-
-    except Exception as e:
-        print(e)
+        routes = data.get('items', [])
         varstr = ""
-        result["admin_url"] = varstr
+        for route in routes:
+            if f'{workspaceId}-all.{instid}' in route['spec']['host']:
+                varstr = route['spec']['host']
+                break
+        else:
+            print(json.dumps({
+                "error": f"No route found for '{workspaceId}-all.{instid}'"
+            }))
+            return
+
+        result = {
+            "admin_url": f"https://{varstr}/maximo"
+        }
         json_output = json.dumps(result)
         print(json_output)
+
+    except json.JSONDecodeError as e:
+        error = {"error":f"Error: Failed to parse JSON: {e}"}
+        json_error = json.dumps(error)
+        print(json_error)
+        
+
+    except subprocess.CalledProcessError as e:
+        error = {
+            "error": f"Error: Command '{e.cmd}' returned non-zero exit status {e.returncode}"
+        }
+        json_error = json.dumps(error)
+        print(json_error)
+        
+    except OSError as e:
+        error = {
+            "error" : f"Error: Failed to execute command: {e}"
+        }
+        json_error = json.dumps(error)
+        print(json_error)
 
 
 if __name__ == "__main__":
@@ -112,15 +119,13 @@ if __name__ == "__main__":
     input_json = json.loads(sys.stdin.read())
 
     # get the KUBECONFIG path from the json
-    kubeconfig = input_json["KUBECONFIG"]
+    kubeconfig = input_json['KUBECONFIG']
 
     capability = sys.argv[1]
     instanceId = sys.argv[2]
     workspaceId = sys.argv[3]
-
+    
     if capability == "core":
         getAdminURLCore(kube_config=kubeconfig, instid=instanceId)
     elif capability == "manage":
-        getAdminURLManage(
-            kube_config=kubeconfig, instid=instanceId, workspaceId=workspaceId
-        )
+        getAdminURLManage(kube_config=kubeconfig, instid=instanceId,workspaceId=workspaceId)
